5c0b7ce6b7633c06a918e5334d753dd57c3be211
Germinary <maxgromov10@yandex.ru>
2024-01-11 22:02:25 +0500

    Second commit

--- main.py
+++ main.py
@@ -7,12 +7,57 @@ db = sqlite3.connect('coffee.sqlite')
 sql = db.cursor()
 
 
+class Edit(QMainWindow):
+    def __init__(self, parent):
+        super().__init__(parent)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+
+        self.pushButton.clicked.connect(self.do)
+        self.lineEdit_2.textChanged.connect(self.refresh)
+
+    def refresh(self):
+        query = "SELECT * FROM coffee WHERE variety_name = ?"
+        name = self.lineEdit_2.text()
+        data = sql.execute(query, (name,)).fetchone()
+        if not (data is None):
+            self.lineEdit_3.setText(data[2])
+            self.lineEdit_4.setText(data[3])
+            self.lineEdit_5.setText(data[4])
+            self.lineEdit_6.setText(data[5])
+            self.lineEdit_7.setText(data[6])
+
+    def do(self):
+        data = []
+        data.append(self.lineEdit_2.text())
+        data.append(self.lineEdit_3.text())
+        data.append(self.lineEdit_4.text())
+        data.append(self.lineEdit_5.text())
+        data.append(self.lineEdit_6.text())
+        data.append(self.lineEdit_7.text())
+
+        try:
+            id = max([i[0] for i in sql.execute("SELECT id FROM coffee").fetchall()]) + 1
+        except ValueError:
+            id = 0
+
+        if sql.execute("SELECT * FROM coffee WHERE variety_name = ?", (data[0],)).fetchone() is None:
+            data = [id] + data
+            sql.execute("INSERT INTO coffee VALUES (?, ?, ?, ?, ?, ?, ?)", data)
+            db.commit()
+        else:
+            query = "UPDATE coffee SET degree_of_roasting = ?, ground_or_cereal = ?, flavor_description = ?, price = ?, package_size = ? WHERE variety_name = ?"
+            sql.execute(query, data[1:] + [data[0]])
+            print(data[1:] + [data[0]])
+            db.commit()
+
+
 class Espresso(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
         
         self.button.clicked.connect(self.do)
+        self.button2.clicked.connect(self.edit)
 
     def get_data(self, name):
         data = sql.execute("SELECT * FROM coffee WHERE variety_name = ?", (name,)).fetchone()
@@ -28,6 +73,7 @@ class Espresso(QMainWindow):
         self.label_6.setText('Цена: ' + data[5])
         self.label_7.setText('Объем упаковки: ' + data[6])
 
+
     def do(self):
         name = self.lineEdit.text()
         data = self.get_data(name)
@@ -36,9 +82,17 @@ class Espresso(QMainWindow):
         except TypeError:
             self.place_data(['N/A'] * 7)
 
+    def edit(self):
+        edit = Edit(self)
+        edit.show()
+        
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Espresso()
     window.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec_())
